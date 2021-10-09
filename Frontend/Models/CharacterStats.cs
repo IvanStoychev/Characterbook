03f00fa415a931a91dd5142345c5aa93aab76b76
Ivan Stoychev <ivanstoychev44@gmail.com>
2021-10-10 01:06:41 +0300

    CharacterStats.cs - Class added.

---
+++ Frontend/Models/CharacterStats.cs
@@ -0,0 +1,83 @@
+﻿namespace Frontend.Models
+{
+    public class CharacterStats
+    {
+        public string Name { get; set; }
+
+        public int Height { get; set; }
+
+        public int Weight { get; set; }
+
+        public CharacterSize Size { get; set; }
+
+        public int Age { get; set; }
+
+        public string Sign { get; set; }
+
+        public string God { get; set; }
+
+        public string Race { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public string Alignment { get; set; }
+
+        public string Fame { get; set; }
+
+        public string Sanity { get; set; }
+
+        public string Strength { get; set; }
+
+        public string Dexterity { get; set; }
+
+        public string Constitution { get; set; }
+
+        public string Intelligence { get; set; }
+
+        public string Wisdom { get; set; }
+
+        public string Charisma { get; set; }
+
+        public string Speed { get; set; }
+
+        public string Courage { get; set; }
+
+        public string HitDie { get; set; }
+
+        public string HealthMax { get; set; }
+
+        public string HealthCurrent { get; set; }
+
+        public string ClassCurrent { get; set; }
+
+        public string ClassLevel { get; set; }
+
+        public string ClassHistory { get; set; }
+
+        public string Experience { get; set; }
+
+        public string BonusToHit { get; set; }
+
+        public string BonusParry { get; set; }
+
+        public string BonusDodge { get; set; }
+
+        public string BonusInitiative { get; set; }
+
+        public string BonusDamage { get; set; }
+
+        public string BonusAPR { get; set; }
+
+        public string SaveFortitude { get; set; }
+
+        public string SaveWill { get; set; }
+
+        public string SaveReflex { get; set; }
+
+        public string ResistanceDamage { get; set; }
+
+        public string ResistanceMagic { get; set; }
+
+
+    }
+}
